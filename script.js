3b913e5f9edeaf6fc7b897cfaac6e25eb2d7b303
gusawe <158056800+gusawe@users.noreply.github.com>
2026-04-09 01:04:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
-// Select the list container
 const indexList = document.getElementById('index-list');
 
-// Select all h2 headings
-const headings = document.querySelectorAll('h2');
+// Convert to array and remove last 3
+const headings = Array.from(document.querySelectorAll('h2')).slice(0, -4);
 
 headings.forEach((heading, i) => {
-  // Assign ID if missing
   if (!heading.id) heading.id = `section${i+1}`;
 
-  // Create list item and link
   const li = document.createElement('li');
   const a = document.createElement('a');
   a.href = `#${heading.id}`;
+
   a.textContent = heading.textContent;
 
   li.appendChild(a);
   indexList.appendChild(li);
 });
+
+
+
+const header = document.getElementById('top-index');
+const firstH2 = document.querySelector('h2');
+
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset;
+
+  const h2Top = firstH2.getBoundingClientRect().top;
+
+  if (h2Top > 0) {
+
+    header.style.transform = 'translateY(-100%)';
+  } else {
+
+    if (currentScroll > lastScroll) {
+
+      header.style.transform = 'translateY(-100%)';
+    } else {
+
+      header.style.transform = 'translateY(0)';
+    }
+  }
+
+  lastScroll = currentScroll;
+});
+
 
 function toggleFootnote(id) {
   const note = document.getElementById(id);
